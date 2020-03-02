854d27da5be82e61c2c4c758d8d48ad3c1ea4e75
Ethan Armstrong <nygmatic10@gmail.com>
2020-03-01 21:18:25 -0600

    Remove unused import (oops)

--- src/main/java/com/nygmatic/core/lotuschat/commands/NameChangeCommand.java
+++ src/main/java/com/nygmatic/core/lotuschat/commands/NameChangeCommand.java
@@ -6,7 +6,6 @@ import com.nygmatic.core.lotuschat.chat.Chatter;
 import com.nygmatic.core.lotuschat.util.PlayerDataUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
